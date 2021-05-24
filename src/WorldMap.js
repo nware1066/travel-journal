c8029fcb4889f1f2d0530ef8980f34a9f30a7502
Naomi Ware <nware1066@yahoo.com>
2021-05-24 13:26:17 -0600

    Add onClick function to return country name

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -1,13 +1,18 @@
 import { VectorMap } from "react-jvectormap";
-import {getName} from "country-list"
+import { getName} from "country-list"
 
 function WorldMap() {
+
+    const handleClick = (e, countryCode) => {
+        console.log(getName(countryCode))
+    }
 
     return(
         <div className="map-container">
             <VectorMap
          map={"world_mill"}
          containerStyle={{height: "100vh"}}
+         onRegionClick={handleClick}
        />
         </div>
     )
