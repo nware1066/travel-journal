eed6969bc93bfe47390241793d0f05edde0bced9
Naomi Ware <nware1066@yahoo.com>
2021-05-25 18:44:08 -0600

    Pass function to get countries names as prop from App.js

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -1,20 +1,39 @@
 import { VectorMap } from "react-jvectormap";
 import { getName} from "country-list"
 
-function WorldMap() {
+function WorldMap(props) {
 
-    const handleClick = (e, countryCode) => {
-        console.log(getName(countryCode))
-    }
+  const handleClick = (e, countryCode) => {
+      console.log(getName(countryCode))
+      props.getCountryName(e, countryCode)
+  }
 
     return(
-        <div className="map-container">
             <VectorMap
          map={"world_mill"}
          containerStyle={{height: "100vh"}}
+           regionStyle={{
+              initial: {
+                    fill: 'aliceblue',
+                    "fill-opacity": 1,
+                    stroke: 'none',
+                    "stroke-width": 0,
+                    "stroke-opacity": 1
+                },
+                hover: {
+                    "fill-opacity": 0.8,
+                    cursor: 'pointer'
+                },
+                selected: {
+                    fill: 'seagreen'
+                },
+                selectedHover: {
+                }
+            }}
          onRegionClick={handleClick}
+         regionsSelectable={true}
+       
        />
-        </div>
     )
 }
 
