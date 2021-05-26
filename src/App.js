fda22d5a82f263d9e867f9a4ccbfb986ff111d1c
Naomi Ware <nware1066@yahoo.com>
2021-05-25 18:43:19 -0600

    Add hooks to manage array of selected countries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-// import {getCode, getName} from "country-list"
+import { getName } from "country-list"
+import { useState } from 'react';
 import './App.css';
 import WorldMap from './WorldMap'
 
 function App() {
 
+    const [countryNames, setCountryNames] = useState([])
+
+      function getCountryName (e, countryCode) {
+       let newCountry = getName(countryCode)
+        console.log(newCountry)
+        setCountryNames(countryNames => {
+         return [...countryNames, newCountry]
+        })
+    }
+ 
+ 
+
+
   return (
     <div className="App">
       <div className="main-container">
-       <WorldMap />
+      <div className="map-container">
+        <WorldMap
+        getCountryName={getCountryName}
+         />
+      </div>
+      <div className="journal-container">
+        <h1>{countryNames}</h1>
+      </div>
       </div>
     </div>
   );
