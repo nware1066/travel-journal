ad9c2adf6646f9ce8d88e69b3d977cfcc772be1a
Naomi Ware <nware1066@yahoo.com>
2021-06-09 18:12:23 -0600

    Create Login file boilerplate

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="login-container">
+        <form action="">
+            <input type="text" className="login login-input"/>
+            <input type="text" className="login login-input"/>
+            <input type="text" className="login login-input"/>
+            <button type="submit" className="login login-button" ></button>
+        </form>
+    </div>
+  )
+}
+
+export default Login
