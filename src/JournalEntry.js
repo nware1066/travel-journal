50d26042a3754365f62a3cbd6a4214621eb96102
Naomi Ware <nware1066@yahoo.com>
2021-06-09 18:11:37 -0600

    Create JournalEntry boilerplate

---
+++ src/JournalEntry.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function JournalEntry() {
+  return (
+    <div className="journal-container">  
+      <h1>Country Name</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum possimus maiores, veritatis alias quaerat fugit blanditiis amet perspiciatis? Consequuntur quasi iure, praesentium reprehenderit voluptates repudiandae explicabo! Debitis incidunt laborum quam qui nostrum atque beatae officiis.</p>
+    </div>
+  )
+}
+
+export default JournalEntry
