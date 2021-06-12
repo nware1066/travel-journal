d1a26684c437ac230da88189d84983a5bb8b5b88
Naomi Ware <nware1066@yahoo.com>
2021-06-12 17:35:20 -0600

    Add jsx for Login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,13 +2,32 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className="login-container">
-        <form action="">
-            <input type="text" className="login login-input"/>
-            <input type="text" className="login login-input"/>
-            <input type="text" className="login login-input"/>
-            <button type="submit" className="login login-button" ></button>
+    <div>
+      <div class="forms-wrapper">
+        <label for="show-hide-forms" >&#10005;</label>
+        <input type="checkbox" id="show-form" h />
+        <div class="show-btns-wrapper">
+            <label for="show-form" class="show-form-btn show-signup-form">Sign Up</label>
+            <label for="show-form" class="show-form-btn show-login-form">Log In</label>
+        </div>
+        <form class="signup-form">
+            <h3 class="form-heading">Sign Up</h3>
+            <div class="fullname-inputs-wrapper">
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Last Name" />
+            </div>
+            <input type="text" class="form-input" placeholder="Username" />
+            <input type="email" class="form-input" placeholder="Email Address" />
+            <input type="password" class="form-input" placeholder="Password" />
+            <input type="submit" value="SIGN UP" class="form-btn" />
         </form>
+        <form class="login-form">
+            <h3 class="form-heading">Log In</h3>
+            <input type="email" class="form-input" placeholder="Email Address" />
+            <input type="password" class="form-input" placeholder="Password" />
+            <input type="submit" value="LOG IN" class="form-btn" />
+        </form>
+      </div>
     </div>
   )
 }
