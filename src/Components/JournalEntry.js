3bde98c59a27c06ec78879d6af77076869e19db1
Naomi Ware <nware1066@yahoo.com>
2021-06-15 16:04:17 -0600

    Move setTrip and handleChange functions to JournalEntry and pass as props to CreateEntry

--- src/Components/JournalEntry.js
+++ src/Components/JournalEntry.js
@@ -6,73 +6,36 @@ import CreateEntry from './CreateEntry'
 function JournalEntry(props) {
   const [showForm, setShowForm] = useState(false);
 
+  const [trip, setTrip] = useState({
+    date: "",
+    name: "",
+    content: "",
+    images: [],
+  });
 
-//   function createTrip(event, index) {
-//     props.addTrip(trip);
-//     setTrip({
-//       date: "",
-//       name: "",
-//       content: "",
-//       pictures: [],
-//       key: { index },
-//       id: { index },
-//     });
-//     event.preventDefault();
-//   }
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+
+    setTrip((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
 
   return (
     <div className="create-entry-wrapper">
       <button onClick={() => setShowForm(!showForm)}>edit journal</button>
-      {showForm ? <CreateEntry /> : null}
+      {showForm ? <CreateEntry 
+        handleFormChange={handleFormChange}
+        trip={trip}
+      /> : null}
 
       <div className="journal-entry">
         <div className="journal-text">
-          <h1>England</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo vitae
-            ipsa vel minima debitis voluptatum labore molestias asperiores
-            beatae inventore!
-            <br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
-            cupiditate neque quod commodi repudiandae velit repellat ea aut
-            distinctio, harum quasi pariatur fugiat inventore officia debitis
-            nihil aperiam, incidunt aspernatur!
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            vitae sit deserunt? Fugiat quisquam non distinctio nesciunt eum,
-            atque culpa est eveniet error odit dicta! Libero, et minus
-            exercitationem veritatis voluptas necessitatibus vero fugit nisi!
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aut
-            voluptas, rerum delectus quaerat neque doloremque ipsum aliquid
-            provident eius excepturi praesentium ea odit sed cum fuga ducimus
-            laboriosam. Nemo, debitis explicabo pariatur a quae dolorem ab
-            laborum vitae totam sapiente fugiat magni ullam veniam eos,
-            laboriosam commodi saepe vel nihil. Eum eos reprehenderit fugiat,
-            tempore tempora consequuntur libero, doloremque quod adipisci quidem
-            odit temporibus labore non aperiam suscipit. Ex deserunt culpa
-            praesentium, laudantium vero ad! Dolor impedit perspiciatis
-            quisquam. Labore aperiam quidem architecto ea accusamus quia, optio
-            dolor nulla autem, distinctio officia natus. Eveniet, aut laboriosam
-            in explicabo unde totam quae labore provident tempore numquam
-            repellat eligendi suscipit illo odit fugit ipsa? Fuga accusamus
-            nihil, quisquam quaerat autem qui harum. Eaque ab reprehenderit unde
-            obcaecati vel qui blanditiis, architecto, nisi, quae rem ullam ipsa
-            adipisci laboriosam dolore voluptates doloribus modi illum quo
-            iusto. Error doloribus earum nostrum illum maxime aliquid fuga
-            assumenda quis explicabo ut quisquam exercitationem qui nisi aut
-            laudantium et quidem, quasi, eius modi. Provident reiciendis
-            facilis, placeat tempora autem hic soluta modi animi repudiandae
-            aspernatur facere quas non, corporis dignissimos odit! Obcaecati
-            debitis optio quam suscipit. Quidem vitae sed ullam veniam incidunt
-            nemo fugit, nulla exercitationem minima, cupiditate voluptatibus
-            libero, quas assumenda rem ab! Perspiciatis repellendus fuga
-            doloremque culpa sint accusantium est neque debitis, excepturi
-            laboriosam dolore nam eaque deleniti totam quod qui corrupti magni
-            dolores nobis numquam? Aspernatur accusantium illo maxime nostrum
-            mollitia exercitationem animi iure reprehenderit iusto! Repellat
-            molestiae laborum ratione sint maxime. Fugit.
-          </p>
+          <h1>{trip.name}</h1>
+          <p>{trip.content}</p>
         </div>
         <div className="gallery">
           <img
