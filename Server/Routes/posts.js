a7451a46618e65fad21a577fc929de24d300f5bb
Naomi Ware <nware1066@yahoo.com>
2021-06-17 16:41:42 -0600

    move functionality for getPosts to controllers/posts

--- Server/Routes/posts.js
+++ Server/Routes/posts.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getPosts } from '../controllers/posts.js'
 
 export const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("travel ")
-})
+router.get('/', getPosts)
 
 export default router;
